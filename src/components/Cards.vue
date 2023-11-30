0077ad26d32500329daf337932043a426c598806
Ming Fai Cheung <mingfai@live.com>
2023-11-30 23:15:18 +0800

    first commit

---
+++ src/components/Cards.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+
+</script>
+
+<script setup lang="ts">
+import {GENDER, type Invitee} from "../types"
+
+defineProps<{
+    invitee: Invitee
+}>()
+
+</script>
+<template>
+    <div :style="{backgroundColor: invitee.gender===GENDER.MALE?'rgb(0,128,128)':'rgb(111,18,8)'}">
+        {{invitee.name}}
+    </div>
+</template>
+
+<style scoped>
+div {
+    background-color: aqua;
+    border-radius: 5px;
+    margin-top: 5px;
+    text-align: center;
+    padding: 3px;
+    color: white;
+}
+
+</style>
